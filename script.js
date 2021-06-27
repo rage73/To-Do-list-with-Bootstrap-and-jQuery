852ec14de87c884478f875fe445af2694e76e1e2
rage73 <raghavagrawal2000ra@gmail.com>
2021-06-27 23:25:10 +0530

    dynamic buttons

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let btnSort = $('#btnSort');
 let inpNewTask = $('#inpNewTask');
 
 function addTask() {
-    if(inpNewTask.val() == ''){
+    if (inpNewTask.val() == '') {
         inpNewTask.focus();
         return;
     }
@@ -16,19 +16,41 @@ function addTask() {
     });
     liNewTask.click((event) => {
         liNewTask.toggleClass('done');
+        toggleInpBtns();
     });
     ulTasks.append(liNewTask);
     inpNewTask.val('').focus();
+    toggleInpBtns();
+}
+
+function toggleInpBtns() {
+    btnReset.prop('disabled', inpNewTask.val() == '');
+    btnAdd.prop('disabled', inpNewTask.val() == '');
+    btnSort.prop('disabled', $('#ulTasks .done').length < 1);
+    btnCleanup.prop('disabled', $('#ulTasks .done').length < 1);
 }
 
 inpNewTask.keypress((e) => {
     if (e.which == 13) addTask();
 });
 
+inpNewTask.on('input', () => {
+    toggleInpBtns();
+});
+
 btnAdd.click(addTask);
 
-btnReset.click(() => inpNewTask.val('').focus());
+btnReset.click(() => {
+    inpNewTask.val('').focus();
+    toggleInpBtns();
+});
 
-btnCleanup.click(() => $('#ulTasks .done').remove());
+btnCleanup.click(() => {
+    $('#ulTasks .done').remove()
+    toggleInpBtns();
+});
 
-btnSort.click(() => $('#ulTasks .done').appendTo(ulTasks));
+btnSort.click(() => {
+    $('#ulTasks .done').appendTo(ulTasks)
+    toggleInpBtns();
+});
